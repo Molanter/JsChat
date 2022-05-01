0f9832262d30a4bab137c54ed6689e63d2062424
Molanter <62011484+Molanter@users.noreply.github.com>
2022-05-01 22:17:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var messages = document.getElementById('messages");
+var sendButton = document.getElementById("send-btn"):
+ornpaaHTh
+sendButton.addEventListener(*click*,sendUserMessage);
+getMessagesFromServer();
+3D-MOnenT
+7-Zip
+Adobe Acrobat DC
+BlueStacks 5
+BlueStacks 5 Multi-Instance Manager
+BlueStacks X
+19
+Chrome Apps
+Cortana
+sync function getMessagesFromServer0)
+var response = await fetch(*https://fchatiavi.herokuapp
+eroN3 a o6b@r lavascripi
+response = await response. json();
+var allMessagesHTML
+for (var 1 = 0; i < response. length; i++) (
+var messageData = responselil;
+11 CosnaT6 BeDCTAY MeceRTa
+var message =
+<div class="message">
+<div class="message-nickname"> (messageData.Name) </divs
+‹div class="message-text"> S{messageData.Message)
+</div>
+</divs
+allMessagesHTML = alIMessagesHTML + message;
